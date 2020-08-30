91ce650d250ed8be48e576385c06a5757381a023
Arina <arinamirosha@gmail.com>
2020-08-30 15:56:54 +0300

    еще какое-то изменение

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-12 text-center p-3 h1">
-            Искать жилье сти
+            Искать жилье стин
         </div>
     </div>
     <div class="row justify-content-center">
