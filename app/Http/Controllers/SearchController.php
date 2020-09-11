650390ea47d9679022cc5f5be5dd2c4c3b313c48
Arina <arinamirosha@gmail.com>
2020-09-11 19:11:06 +0300

    correct search query, usage of whereDoesntHave()

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -13,46 +13,38 @@ class SearchController extends Controller
 {
     public function index(SearchRequest $request)
     {
-        $data = $request->all();
+        $requestData = $request->all();
 
         $houses = House::addSelect(['user_name' => User::select('name')->whereColumn('user_id', 'users.id')])
-            ->where('city', 'like', "%{$data['where']}%")
-            ->where('places', '>=', $data['people'])
+            ->where('city', 'like', "%{$requestData['where']}%")
+            ->where('places', '>=', $requestData['people'])
+            ->whereDoesntHave('bookings', function ($query) use ($requestData) {
+                $query
+                    // также в HousesController@show на $isFree
+                    ->where('status', '=', Booking::STATUS_BOOKING_ACCEPT)
+                    ->where(function ($query) use ($requestData) {
+                        $query
+                            ->whereBetween('departure', [$requestData['arrival'], $requestData['departure']])
+                            ->orWhereBetween('arrival', [$requestData['arrival'], $requestData['departure']]);
+                    });
+            })
             ->orderBy('name')
             ->orderBy('user_name')
+            ->with('user')
             ->get();
 
         session([
-            'arrival' => $data['arrival'],
-            'departure' => $data['departure'],
-            'people' => $data['people'],
+            'arrival' => $requestData['arrival'],
+            'departure' => $requestData['departure'],
+            'people' => $requestData['people'],
         ]);
-
-        // эта же проверка должна быть в HousesController@show на $isFree, но с !
-        $ids = [];
-        foreach ($houses as $house) {
-            // найти лишее, уже бронь на эти даты
-            if ($house->bookings()
-                ->where('status', '=', Booking::STATUS_BOOKING_ACCEPT)
-                ->where(function ($query){
-                    $query
-                    ->whereBetween('departure', [session('arrival'), session('departure')])
-                    ->orWhereBetween('arrival', [session('arrival'), session('departure')]);
-                })
-                ->exists()
-            )
-                array_push($ids, $house->id);
-        }
-        // исключить
-        $houses = $houses->whereNotIn('id', $ids);
-        $houses = $houses->load('user');
 
         return view('search', [
             'houses' => $houses,
-            'where' => $data['where'],
-            'arrival' => $data['arrival'],
-            'departure' => $data['departure'],
-            'people' => $data['people']
+            'where' => $requestData['where'],
+            'arrival' => $requestData['arrival'],
+            'departure' => $requestData['departure'],
+            'people' => $requestData['people']
         ]);
     }
 }
