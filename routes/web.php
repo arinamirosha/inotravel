4280f93a3bb25b6ccbbb9f15f0e1d035e42b20d4
Arina <arinamirosha@gmail.com>
2020-09-11 11:37:41 +0300

    correct get/post

--- routes/web.php
+++ routes/web.php
@@ -22,22 +22,23 @@ Route::post('/profile/{user}', 'ProfilesController@update')->name('profile.updat
 Route::post('/profile/{user}/update-password', 'ProfilesController@updatePassword')->name('profile.update_password');
 
 Route::get('/house/create', 'HousesController@create')->name('house.create');
-Route::post('/house', 'HousesController@store')->name('house.store');
 Route::get('/house', 'HousesController@index')->name('house.index');
 Route::get('/house/{house}', 'HousesController@show')->name('house.show');
-Route::post('/house/{house}', 'HousesController@destroy')->name('house.destroy');
 Route::get('/house/{house}/edit', 'HousesController@edit')->name('house.edit');
-Route::post('/house/{house}', 'HousesController@update')->name('house.update');
+Route::post('/house', 'HousesController@store')->name('house.store');
+Route::post('/house/{house}/update', 'HousesController@update')->name('house.update');
+Route::post('/house/{house}', 'HousesController@destroy')->name('house.destroy');
 
 Route::get('/search', 'SearchController@index')->name('search');
 
-Route::post('/booking', 'BookingController@store')->name('booking.store');
 Route::get('/booking', 'BookingController@index')->name('booking.index');
-Route::post('/booking/{booking}', 'BookingController@update')->name('booking.update');
+Route::post('/booking', 'BookingController@store')->name('booking.store');
+Route::post('/booking/{booking}/update', 'BookingController@update')->name('booking.update');
 Route::post('/booking/{booking}', 'BookingController@destroy')->name('booking.destroy');
 
-// нет пути
-Route::get('/profile/{user}', function ($user) { return redirect(route('profile.edit', $user)); }); // из браузера идет не в patch
-Route::get('/profile/{user}/update-password', function ($user) { return redirect(route('profile.edit', $user)); }); // из браузера идет не в patch
-Route::get('/booking/{booking}', function () { return redirect(route('welcome')); }); // из браузера идет не в patch
-
+// идет не в post
+Route::get('/profile/{user}', function ($user) { return redirect(route('profile.edit', $user)); });
+Route::get('/profile/{user}/update-password', function ($user) { return redirect(route('profile.edit', $user)); });
+Route::get('/house/{house}/update', function () { return redirect(route('house.index')); });
+Route::get('/booking/{booking}/update', function () { return redirect(route('booking.index')); });
+Route::get('/booking/{booking}', function () { return redirect(route('booking.index')); });
