5ccb28dad2eac6c245724ab8a6f2d78f4d52fdf2
Arina <arinamirosha@gmail.com>
2020-09-13 19:02:58 +0300

    forget cookies when logout

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,6 +5,9 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
 
 class LoginController extends Controller
 {
@@ -36,5 +39,13 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function logout(Request $request) {
+        Auth::logout();
+        Cookie::queue(Cookie::forget('arrival'));
+        Cookie::queue(Cookie::forget('departure'));
+        Cookie::queue(Cookie::forget('people'));
+        return redirect('/login');
     }
 }
