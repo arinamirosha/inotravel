8476997638dcfe54bb29eccd3fa934e85c014601
Arina <arinamirosha@gmail.com>
2020-09-24 18:37:46 +0300

    correct search in House model isFree() method

--- app/House.php
+++ app/House.php
@@ -7,6 +7,7 @@ use App\Mail\BookingDeletedNotification;
 use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Storage;
 
@@ -125,25 +126,47 @@ class House extends Model
 //        })
 //        ->exists();
 
-        $isFree = true;
-        for ($i = Carbon::parse($arrival); $i <= Carbon::parse($departure); $i = $i->add(1, 'day')) {
+//        $isFree = true;
+//        for ($i = Carbon::parse($arrival); $i <= Carbon::parse($departure); $i = $i->add(1, 'day')) {
+//
+//            $date = $i->format('Y-m-d');
+//
+//            $full = (
+//                    $this->bookings()
+//                        ->where('status', '=', Booking::STATUS_BOOKING_ACCEPT)
+//                        ->where('arrival', '<=', $date)
+//                        ->where('departure', '>=', $date)
+//                        ->sum('people')
+//                    + $people
+//                ) > $this->places;
+//
+//            if ($full) {
+//                $isFree = false;
+//                break;
+//            }
+//        }
 
-            $date = $i->format('Y-m-d');
-
-            $full = (
-                    $this->bookings()
-                        ->where('status', '=', Booking::STATUS_BOOKING_ACCEPT)
-                        ->where('arrival', '<=', $date)
-                        ->where('departure', '>=', $date)
-                        ->sum('people')
-                    + $people
-                ) > $this->places;
-
-            if ($full) {
-                $isFree = false;
-                break;
-            }
-        }
+        $isFree = ! DB::table(DB::raw('(select * from
+		 (select adddate(\''.$arrival.'\',t1*10 + t0) gen_date from
+		 (select 0 t0 union select 1 union select 2 union select 3 union select 4 union select 5 union select 6 union select 7 union select 8 union select 9) t0,
+		 (select 0 t1 union select 1 union select 2 union select 3 union select 4 union select 5) t1) v
+		 where gen_date between \''.$arrival.'\' and \''.$departure.'\') as t'))
+            ->select([
+                DB::raw('SUM(people) AS summa'),
+                't.gen_date',
+                'bookings.house_id',
+            ])
+            ->leftJoin('bookings', function ($query) {
+                $query->on(DB::raw('1'), '=', DB::raw('1'));
+            })
+            ->addSelect(['places' => House::select('places')->whereColumn('house_id', 'houses.id')])
+            ->where('bookings.house_id', '=', $this->id)
+            ->whereColumn('arrival', '<=', 't.gen_date')
+            ->whereColumn('departure', '>=', 't.gen_date')
+            ->where('status', Booking::STATUS_BOOKING_ACCEPT)
+            ->groupBy(DB::raw('bookings.house_id, t.gen_date'))
+            ->having(DB::raw('places-summa'), '<', $people)
+            ->exists();
 
         return $isFree;
     }
