f934bc1f81fef8a44679defdc09a4d9fb6030bfc
Arina <arinamirosha@gmail.com>
2020-09-29 16:02:37 +0300

    email constructor

--- resources/views/email/booking_changed_notification.blade.php
+++ resources/views/email/booking_changed_notification.blade.php
@@ -1,29 +1,32 @@
-@extends('layouts.email')
-
-@section('content')
-
-    <div class="row text-center mt-4">
-        <div class="col-12 h3">
-            <div>
-                Новый статус заявки
-            </div>
-            <div>
-                с <b>{{$booking->arrival}}</b> по <b>{{$booking->departure}}</b>:
-            </div>
-            <div class="font-weight-bold m-3">
-                @switch($booking->status)
-                    @case(\App\Booking::STATUS_BOOKING_ACCEPT)ОДОБРЕНО@break
-                    @case(\App\Booking::STATUS_BOOKING_REJECT)ОТКАЗ@break
-                    @case(\App\Booking::STATUS_BOOKING_CANCEL)ОТМЕНА@break
-                @endswitch
-            </div>
-            <div>
-                Название: <b>{{$booking->house->name}}</b>
-            </div>
-            <div>
-                Город: <b>{{$booking->house->city}}</b>
-            </div>
+@component('mail::layout')
+    {{-- Header --}}
+    @slot('header')
+        @component('mail::header', ['url' => config('app.url')])
+            {{ config('app.name') }}
+        @endcomponent
+    @endslot
+    {{-- Body --}}
+    @component('mail::panel')
+        <div>
+            Новый статус заявки
         </div>
-    </div>
-
-@endsection
+        <div>
+            с <b>{{$booking->arrival}}</b> по <b>{{$booking->departure}}</b>:
+        </div>
+        <div class="font-weight-bold m-3">
+            @switch($booking->status)
+                @case(\App\Booking::STATUS_BOOKING_ACCEPT)ОДОБРЕНО@break
+                @case(\App\Booking::STATUS_BOOKING_REJECT)ОТКАЗ@break
+                @case(\App\Booking::STATUS_BOOKING_CANCEL)ОТМЕНА@break
+            @endswitch
+        </div>
+        Название: <b>{{$booking->house->name}}</b><br>
+        Город: <b>{{$booking->house->city}}</b>
+    @endcomponent
+    {{-- Footer --}}
+    @slot('footer')
+        @component('mail::footer')
+            © {{ date('Y') }} {{ config('app.name') }}. @lang('All rights reserved.')
+        @endcomponent
+    @endslot
+@endcomponent
