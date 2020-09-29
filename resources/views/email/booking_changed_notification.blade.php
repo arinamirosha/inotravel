52cb409f409da1ab7b407242680733c335e7f45a
Arina <arinamirosha@gmail.com>
2020-09-29 12:54:51 +0300

    stylization of emails

--- resources/views/email/booking_changed_notification.blade.php
+++ resources/views/email/booking_changed_notification.blade.php
@@ -2,19 +2,28 @@
 
 @section('content')
 
-    <div class="row">
-        <div class="col-8 h3">
-            <div>Новый статус заявки с <b>{{$booking->arrival}}</b> по <b>{{$booking->departure}}</b>:
-                <b>
-                    @switch($booking->status)
-                        @case(\App\Booking::STATUS_BOOKING_ACCEPT)ОДОБРЕНО@break
-                        @case(\App\Booking::STATUS_BOOKING_REJECT)ОТКАЗ@break
-                        @case(\App\Booking::STATUS_BOOKING_CANCEL)ОТМЕНА@break
-                    @endswitch
-                </b>
+    <div class="row text-center mt-4">
+        <div class="col-12 h3">
+            <div>
+                Новый статус заявки
             </div>
-            <div>Название: <b>{{$booking->house->name}}</b></div>
-            <div>Город: <b>{{$booking->house->city}}</b></div>
+            <div>
+                с <b>{{$booking->arrival}}</b> по <b>{{$booking->departure}}</b>:
+            </div>
+            <div class="font-weight-bold m-3">
+                @switch($booking->status)
+                    @case(\App\Booking::STATUS_BOOKING_ACCEPT)ОДОБРЕНО@break
+                    @case(\App\Booking::STATUS_BOOKING_REJECT)ОТКАЗ@break
+                    @case(\App\Booking::STATUS_BOOKING_CANCEL)ОТМЕНА@break
+                @endswitch
+            </div>
+            <div>
+                Название: <b>{{$booking->house->name}}</b>
+            </div>
+            <div>
+                Город: <b>{{$booking->house->city}}</b>
+            </div>
         </div>
     </div>
+
 @endsection
--- resources/views/layouts/email.blade.php
+++ resources/views/layouts/email.blade.php
@@ -4,26 +4,289 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <!-- CSRF Token -->
-    <meta name="csrf-token" content="{{ csrf_token() }}">
-
-    <title>{{ config('app.name', 'Laravel') }}</title>
-
-    <!-- Scripts -->
-    <script src="{{ asset('js/app.js') }}" defer></script>
-
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <style>
+
+        body {
+            margin: 0;
+            font-family: "Nunito", sans-serif;
+            font-size: 0.9rem;
+            font-weight: 400;
+            line-height: 1.6;
+            color: #212529;
+            text-align: left;
+            background-color: #f8fafc;
+        }
+
+        .font-weight-bold, b {
+            font-weight: 700 !important;
+        }
+
+        .m-1 {
+            margin: 0.25rem !important;
+        }
+
+        .mt-1,
+        .my-1 {
+            margin-top: 0.25rem !important;
+        }
+
+        .mr-1,
+        .mx-1 {
+            margin-right: 0.25rem !important;
+        }
+
+        .mb-1,
+        .my-1 {
+            margin-bottom: 0.25rem !important;
+        }
+
+        .ml-1,
+        .mx-1 {
+            margin-left: 0.25rem !important;
+        }
+
+        .m-2 {
+            margin: 0.5rem !important;
+        }
+
+        .mt-2,
+        .my-2 {
+            margin-top: 0.5rem !important;
+        }
+
+        .mr-2,
+        .mx-2 {
+            margin-right: 0.5rem !important;
+        }
+
+        .mb-2,
+        .my-2 {
+            margin-bottom: 0.5rem !important;
+        }
+
+        .ml-2,
+        .mx-2 {
+            margin-left: 0.5rem !important;
+        }
+
+        .m-3 {
+            margin: 1rem !important;
+        }
+
+        .mt-3,
+        .my-3 {
+            margin-top: 1rem !important;
+        }
+
+        .mr-3,
+        .mx-3 {
+            margin-right: 1rem !important;
+        }
+
+        .mb-3,
+        .my-3 {
+            margin-bottom: 1rem !important;
+        }
+
+        .ml-3,
+        .mx-3 {
+            margin-left: 1rem !important;
+        }
+
+        .m-4 {
+            margin: 1.5rem !important;
+        }
+
+        .mt-4,
+        .my-4 {
+            margin-top: 1.5rem !important;
+        }
+
+        .mr-4,
+        .mx-4 {
+            margin-right: 1.5rem !important;
+        }
+
+        .mb-4,
+        .my-4 {
+            margin-bottom: 1.5rem !important;
+        }
+
+        .ml-4,
+        .mx-4 {
+            margin-left: 1.5rem !important;
+        }
+
+        .m-5 {
+            margin: 3rem !important;
+        }
+
+        .mt-5,
+        .my-5 {
+            margin-top: 3rem !important;
+        }
+
+        .mr-5,
+        .mx-5 {
+            margin-right: 3rem !important;
+        }
+
+        .mb-5,
+        .my-5 {
+            margin-bottom: 3rem !important;
+        }
+
+        .ml-5,
+        .mx-5 {
+            margin-left: 3rem !important;
+        }
+
+        .h1,
+        .h2,
+        .h3,
+        .h4,
+        .h5,
+        .h6 {
+            margin-bottom: 0.5rem;
+            font-weight: 500;
+            line-height: 1.2;
+        }
+
+        h1,
+        .h1 {
+            font-size: 2.25rem;
+        }
+
+        h2,
+        .h2 {
+            font-size: 1.8rem;
+        }
+
+        h3,
+        .h3 {
+            font-size: 1.575rem;
+        }
+
+        h4,
+        .h4 {
+            font-size: 1.35rem;
+        }
+
+        h5,
+        .h5 {
+            font-size: 1.125rem;
+        }
+
+        h6,
+        .h6 {
+            font-size: 0.9rem;
+        }
+
+        .row {
+            display: flex;
+            flex-wrap: wrap;
+            margin-right: -15px;
+            margin-left: -15px;
+        }
+
+        .text-center {
+            text-align: center !important;
+        }
+
+        .col-12,
+        .col-11,
+        .col-10,
+        .col-9,
+        .col-8,
+        .col-7,
+        .col-6,
+        .col-5,
+        .col-4,
+        .col-3,
+        .col-2,
+        .col-1 {
+            position: relative;
+            width: 100%;
+            padding-right: 15px;
+            padding-left: 15px;
+        }
+
+        .col {
+            flex-basis: 0;
+            flex-grow: 1;
+            max-width: 100%;
+        }
+
+        .col-1 {
+            flex: 0 0 8.3333333333%;
+            max-width: 8.3333333333%;
+        }
+
+        .col-2 {
+            flex: 0 0 16.6666666667%;
+            max-width: 16.6666666667%;
+        }
+
+        .col-3 {
+            flex: 0 0 25%;
+            max-width: 25%;
+        }
+
+        .col-4 {
+            flex: 0 0 33.3333333333%;
+            max-width: 33.3333333333%;
+        }
+
+        .col-5 {
+            flex: 0 0 41.6666666667%;
+            max-width: 41.6666666667%;
+        }
+
+        .col-6 {
+            flex: 0 0 50%;
+            max-width: 50%;
+        }
+
+        .col-7 {
+            flex: 0 0 58.3333333333%;
+            max-width: 58.3333333333%;
+        }
+
+        .col-8 {
+            flex: 0 0 66.6666666667%;
+            max-width: 66.6666666667%;
+        }
+
+        .col-9 {
+            flex: 0 0 75%;
+            max-width: 75%;
+        }
+
+        .col-10 {
+            flex: 0 0 83.3333333333%;
+            max-width: 83.3333333333%;
+        }
+
+        .col-11 {
+            flex: 0 0 91.6666666667%;
+            max-width: 91.6666666667%;
+        }
+
+        .col-12 {
+            flex: 0 0 100%;
+            max-width: 100%;
+        }
+
+    </style>
+
 </head>
 <body>
-<div id="app">
-    <main class="py-4">
-        @yield('content')
-    </main>
-</div>
+    @yield('content')
 </body>
 </html>
+
