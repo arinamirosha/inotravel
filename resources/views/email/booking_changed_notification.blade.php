d7af98b7df69e6a29e6cb2a81325b2f1dd0a258d
Arina <arinamirosha@gmail.com>
2020-09-30 12:12:42 +0300

    email blade

--- resources/views/email/booking_changed_notification.blade.php
+++ resources/views/email/booking_changed_notification.blade.php
@@ -2,7 +2,9 @@
     {{-- Body --}}
     @component('mail::panel')
         <div>
-            Новый статус заявки<br>
+            Новый статус заявки
+        </div>
+        <div>
             с <b>{{$booking->arrival}}</b> по <b>{{$booking->departure}}</b>:
         </div>
         <div class="font-weight-bold m-3">
@@ -12,9 +14,11 @@
                 @case(\App\Booking::STATUS_BOOKING_CANCEL)ОТМЕНА@break
             @endswitch
         </div>
-        <div>
-            Название: <b>{{$booking->house->name}}</b><br>
-            Город: <b>{{$booking->house->city}}</b>
-        </div>
+    @endcomponent
+    @component('mail::panel')
+        Название: <b>{{$booking->house->name}}</b>
+    @endcomponent
+    @component('mail::panel')
+        Город: <b>{{$booking->house->city}}</b>
     @endcomponent
 @endcomponent
