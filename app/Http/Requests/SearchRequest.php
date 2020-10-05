3c53e0b1874debc94d686056ee532077bdde206e
Arina <arinamirosha@gmail.com>
2020-10-05 19:13:50 +0300

    correct rule for count of people in search

--- app/Http/Requests/SearchRequest.php
+++ app/Http/Requests/SearchRequest.php
@@ -28,7 +28,7 @@ class SearchRequest extends FormRequest
             'where' => ['required', 'string', 'max:255'],
             'arrival' => ['required', 'date', 'after:yesterday'],
             'departure' => ['required', 'date', 'after:arrival', new TwoMonths($this->input('arrival'))],
-            'people' => ['required', 'numeric', 'max:100'],
+            'people' => ['required', 'numeric', 'max:100', 'min:1'],
         ];
     }
 
@@ -46,6 +46,7 @@ class SearchRequest extends FormRequest
             'people.required' => 'Укажите количество людей',
             'where.max' => 'Город не может содержать > 255 символов',
             'people.max' => 'Людей не может быть > 100',
+            'people.min' => 'Укажите хотя бы 1 человека',
             'people.numeric' => 'Введите число',
             'arrival.after' => 'Прибытие может быть c сегодняшнего дня',
             'departure.after' => 'Отъезд должен быть после даты прибытия',
