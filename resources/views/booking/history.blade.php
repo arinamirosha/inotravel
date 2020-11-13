32142a243ffe80d5ce70790dfcf684a3982ca5bb
arinamirosha <arinamirosha@gmail.com>
2020-11-13 11:52:55 +0300

    translation

--- resources/views/booking/history.blade.php
+++ resources/views/booking/history.blade.php
@@ -13,9 +13,12 @@
             @csrf
             @include('inc.filter_form_elements')
 
-            <button type="submit" class="btn btn-block btn-outline-primary">
+            <button type="submit" class="btn btn-block btn-primary">
                 {{__('Apply')}}
             </button>
+            <a class="btn btn-block btn-outline-primary" href="{{route('booking.history')}}">
+                {{__('Clear all filters')}}
+            </a>
             <a class="btn btn-block btn-outline-primary" href="#" onclick="return confirm('{{ __('Are you sure you want to clear history?') }}')">
                 {{__('Clear history')}}
             </a>
