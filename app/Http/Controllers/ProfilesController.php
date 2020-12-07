a49588864c97c4cb2ad7edf7706ed22906588863
arinamirosha <arinamirosha@gmail.com>
2020-12-07 16:26:09 +0300

    show profile without auth

--- app/Http/Controllers/ProfilesController.php
+++ app/Http/Controllers/ProfilesController.php
@@ -20,7 +20,7 @@ class ProfilesController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth');
+        $this->middleware('auth')->except('show');
     }
 
     /**
