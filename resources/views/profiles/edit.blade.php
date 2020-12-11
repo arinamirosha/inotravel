e699cb9d096bd30868490c30aca1d626fee73421
arinamirosha <arinamirosha@gmail.com>
2020-12-11 14:49:05 +0300

    usage of bootstrap icons

--- resources/views/profiles/edit.blade.php
+++ resources/views/profiles/edit.blade.php
@@ -14,8 +14,10 @@
                     <div class="row">
 
                         <div class="col-2 col-md-3 col-lg-2">
-                            <a class="navbar-item" href="{{ route('profile.show', $user->id) }}">
-                                <img src="{{url('/images/back.png')}}" class="w-100" title="{{__('Back to profile')}}">
+                            <a class="navbar-item" href="{{ route('profile.show', $user->id) }}" title="{{__('Back to profile')}}">
+                                <svg width="2em" height="2em" viewBox="0 0 16 16" class="bi bi-chevron-left" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                    <path fill-rule="evenodd" d="M11.354 1.646a.5.5 0 0 1 0 .708L5.707 8l5.647 5.646a.5.5 0 0 1-.708.708l-6-6a.5.5 0 0 1 0-.708l6-6a.5.5 0 0 1 .708 0z"/>
+                                </svg>
                             </a>
                         </div>
 
